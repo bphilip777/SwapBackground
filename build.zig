e11297c6a945c87995d3a1c8fb600710a1035511
IdiotAtWork <bphilip777@gmail.com>
2025-09-10 10:10:46 -0600

    Updated name to match repo

--- build.zig
+++ build.zig
@@ -4,13 +4,13 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
     const exe = b.addExecutable(.{
-        .name = "CycleBackground",
+        .name = "SwapBackground",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
             .target = target,
             .optimize = optimize,
             .imports = &.{
-                // .{ .name = "CycleBackground", .module = mod },
+                // .{ .name = "SwapBackground", .module = mod },
             },
         }),
     });
